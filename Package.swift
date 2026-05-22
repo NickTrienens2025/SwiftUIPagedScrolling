223a9d4acabd1adce44e18782d03151e273d99ad
Nick Trienens <nicktrienens@gmail.com>
2026-05-21 21:35:01 -0600

    ci: Add GitHub Actions workflow to auto-publish DocC documentation

--- Package.swift
+++ Package.swift
@@ -16,6 +16,9 @@ let package = Package(
             targets: ["SwiftUIPagedScrolling"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
+    ],
     targets: [
         .target(
             name: "SwiftUIPagedScrolling"
